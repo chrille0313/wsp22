75dd65bf5c0b9e9d523a116e6605413c2b531291
christian-mattsson-nti-johanneberg <christian.mattsson@elev.ga.ntig.se>
2022-02-05 14:00:04 +0100

    Project Setup

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require 'slim'
 require 'sqlite3'
 
 
+def connect_to_db(name, rootDir="db")
+    db = SQLite3::Database.new("#{rootDir}/#{name}.db")
+    db.results_as_hash = true
+    return db
+end
+
+
+get('/')  do
+  slim(:home)
+end
