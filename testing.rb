32d86894b3a04010db45fe4c3c434b5c10871ac0
Christian <74824121+christian-mattsson-nti-johanneberg@users.noreply.github.com>
2022-04-26 02:15:51 +0200

    Update testing.rb

--- testing.rb
+++ testing.rb
@@ -1,6 +1,23 @@
+def password_is_strong(password)
+    # ^                 Start anchor
+    # (?=.*[a-z])       Ensure string has one lowercase letter.
+    # (?=.*[A-Z])       Ensure string has one uppcercase letter.
+    # (?=.*[0-9])       Ensure string has one digit.
+    # (?=.*[!@#$&*_])   Ensure string has one special case letter.
+    # (?=.{8,})         Ensure string has atleast 8 characters.
 
-x = 0
-
-if 0
-    p "aoinaosd"
+    return (password =~ /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_])(?=.{8,})/) != nil
 end
+
+def email_is_valid(email)
+    return (email =~ /([-!#-'*+-9=?A-Z^-~]+(\.[-!#-'*+-9=?A-Z^-~]+)*)@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/) != nil
+end
+
+def is_empty(str)
+    return (str =~ /^.*[^\s].*$/) == nil
+end
+
+
+x = "    "
+
+p is_empty(x)
