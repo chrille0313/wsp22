efd12f97233d2f1e598b60e9da92822c5cdce0a6
Christian <74824121+christian-mattsson-nti-johanneberg@users.noreply.github.com>
2022-05-01 02:57:20 +0200

    Update popup.js

--- public/js/popup.js
+++ public/js/popup.js
@@ -2,12 +2,12 @@ var popups = document.querySelectorAll('.popup');
 var popup_triggers = document.querySelectorAll('.popup-trigger');
 
 popup_triggers.forEach((trigger, index) => {
-    trigger.addEventListener('click', (e) => {
+    trigger.addEventListener('click', () => {
         var popup = popups[index]
         popup.classList.add('visible');
 
         var close_button = popup.querySelector('.popup-close');
-        close_button.addEventListener('click', (e) => {
+        close_button.addEventListener('click', () => {
             popup.classList.remove('visible');
         });
     });
